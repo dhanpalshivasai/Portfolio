bb43d32bb10e6e1582c4dbf2197f5fcdbf9c8c98
Dhanpal Shiva Sai <dhanpalshivasai1@gmail.com>
2025-08-10 17:04:19 +0530

    second change

--- src/App.jsx
+++ src/App.jsx
@@ -231,10 +231,6 @@ function App() {
                 <span className="text-gray-700">dhanpalshivasai1@gmail.com</span>
               </div>
               <div className="flex items-center bg-white px-4 py-2 rounded-full shadow-md">
-                <Phone className="w-5 h-5 text-green-600 mr-2" />
-                <span className="text-gray-700">+91 9440599293</span>
-              </div>
-              <div className="flex items-center bg-white px-4 py-2 rounded-full shadow-md">
                 <MapPin className="w-5 h-5 text-red-600 mr-2" />
                 <span className="text-gray-700">Hyderabad, India</span>
               </div>
